45a94df2968d878256fdf74d78aa48266a7bdc47
jy1770 <jy2197352157@gmail.com>
2026-03-19 15:58:14 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-for ModelName in ['src','tgt']:
-    for NIdx in range(6):
-        for ExpertIdx in range(8):
-            os.system(f".\\venv\\Scripts\\python.exe .\\SwitchTransfomer\\main.py experiment --Greedy True --Beam False --num 100000 --GpuNum 1 --batch_size 16 --Occlusion_ModelName {ModelName} --Occlusion_NIdx {NIdx} --Occlusion_ExpertIdx {ExpertIdx}")    
